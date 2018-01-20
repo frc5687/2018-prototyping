1098d448adde96f6223a36a67d2dfcfb111406c3
JCharante <git@jcharante.com>
2018-01-20 17:55:31 -0500

    implement JeVoisProxy

--- src/main/java/org/frc5687/powerup/prototyping/Robot.java
+++ src/main/java/org/frc5687/powerup/prototyping/Robot.java
@@ -1,14 +1,18 @@
 package org.frc5687.powerup.prototyping;
 
 import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.frc5687.powerup.prototyping.subsystems.DriveTrain;
 import com.kauailabs.navx.*;
+import org.frc5687.powerup.prototyping.utils.JeVoisProxy;
+import org.json.*;
 
 public class Robot extends IterativeRobot  {
 
@@ -19,7 +23,8 @@ public class Robot extends IterativeRobot  {
 
     public static DriveTrain driveTrain;
     public static AHRS ahrs;
-    public static SerialPort serialPort;
+    //public static SerialPort serialPort;
+    public static JeVoisProxy jeVoisProxy;
 
     public Robot() {
     }
@@ -37,8 +42,9 @@ public class Robot extends IterativeRobot  {
         driveTrain = new DriveTrain();
 
         oi = new OI();
+        jeVoisProxy = new JeVoisProxy();
 
-        serialPort = new SerialPort(115200, SerialPort.Port.kMXP);
+        //serialPort = new SerialPort(115200, SerialPort.Port.kMXP);
     }
 
     @Override
@@ -64,10 +70,18 @@ public class Robot extends IterativeRobot  {
 
     @Override
     public void robotPeriodic() {
+        /*
         String stringInBuffer = serialPort.readString();
-        if (stringInBuffer.length() > 0) {
+        if (stringInBuffer.length() > 15) {
+            if (stringInBuffer.substring(0, 13).equals("JeVoisTargets")) {
+                String json_string = stringInBuffer.substring(15, stringInBuffer.length());
+                // JSONObject obj = new JSONObject(json_string);
+                // double aX = obj.getDouble("aX");
+                //SmartDashboard.putNumber("JeVois/aX", aX);
+                SmartDashboard.putString("JeVois/json", json_string);
+            }
             DriverStation.reportError(stringInBuffer, true);
-        }
+        }*/
         updateDashboard();
     }
 
@@ -90,6 +104,8 @@ public class Robot extends IterativeRobot  {
     }
 
     public void updateDashboard() {
+        SmartDashboard.putNumber("JeVois/x", jeVoisProxy.GetX());
+        SmartDashboard.putNumber("JeVois/y", jeVoisProxy.GetY());
     }
 
 }
--- src/main/java/org/frc5687/powerup/prototyping/utils/JeVoisProxy.java
+++ src/main/java/org/frc5687/powerup/prototyping/utils/JeVoisProxy.java
@@ -1,6 +1,9 @@
 package org.frc5687.powerup.prototyping.utils;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.frc5687.powerup.prototyping.RobotMap;
 
 /**
  * Created by Ben Bernard on 1/14/2018.
@@ -39,21 +42,46 @@ public class JeVoisProxy {
 
         protected JeVoisListener(JeVoisProxy proxy) {
             this.proxy = proxy;
-            jevoisPort = new SerialPort(15200, SerialPort.Port.kOnboard);
+            jevoisPort = new SerialPort(115200, SerialPort.Port.kMXP);
         }
 
         public void run() {
+            int i = 0;
             while (true) {
                 try {
+                    /*
+                    String data = "JeVoisTargetingInfo: 23;" + Integer.toString(i);//
+                    i++;
+                    */
                     String data = jevoisPort.readString();
-                    String[] a = data.split(";");
+                    if (data.length() == 0) {
+                        continue;
+                    }
+                    String identifier_string = "JeVoisTargetingInfo: ";
+                    // Check to see if the identifier_string prepends our payload
+                    if (data.length() <= identifier_string.length() || !data.substring(0, identifier_string.length()).equals(identifier_string)) {
+                        DriverStation.reportError(String.format("Rejected %s", data), false);
+                        continue;
+                    }
+                    String payload = data.substring(identifier_string.length(), data.length());
+                    //DriverStation.reportError(data, false);
+                    //DriverStation.reportError(data.substring(0, identifier_string.length()), false);
+                    //DriverStation.reportError(payload, false);
+                    /*
+                    if (!data.substring(0, identifier_string.length()).equals(identifier_string)) {
+                        break;
+                    }
+                    data = data.substring(identifier_string.length(), data.length());
+                    */
+                    String[] a = payload.split(";");
                     int x = Integer.parseInt(a[0]);
                     int y = Integer.parseInt(a[1]);
+                    // DriverStation.reportError(String.format("x: %s; y: %s", x, y), false);
                     proxy.Set(x, y);
-
                     Thread.sleep(20);
                 } catch (Exception e) {
-                    break;
+                    continue;
+                    // DriverStation.reportError(e.toString(), true);
                 }
             }
         }
