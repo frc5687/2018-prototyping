9380cdbe0e1b90fa7cf3f39cedfd02e13f524ab0
JCharante <git@jcharante.com>
2018-01-25 17:09:19 -0500

    log getSelectedSensorPosition to dashboard

--- src/main/java/org/frc5687/powerup/prototyping/subsystems/DriveTrain.java
+++ src/main/java/org/frc5687/powerup/prototyping/subsystems/DriveTrain.java
@@ -1,26 +1,29 @@
 package org.frc5687.powerup.prototyping.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.frc5687.powerup.prototyping.Constants;
 import org.frc5687.powerup.prototyping.RobotMap;
 import org.frc5687.powerup.prototyping.commands.DriveWith2Joysticks;
 
-/**
- * Created by Baxter on 3/22/2017.
- */
 public class DriveTrain extends Subsystem {
 
     private VictorSP leftFrontMotor;
     private VictorSP leftRearMotor;
     private VictorSP rightFrontMotor;
     private VictorSP rightRearMotor;
+    private TalonSRX talon;
 
     public DriveTrain() {
         leftFrontMotor = new VictorSP(RobotMap.DriveTrain.LEFT_FRONT_MOTOR);
         leftRearMotor = new VictorSP(RobotMap.DriveTrain.LEFT_REAR_MOTOR);
         rightFrontMotor = new VictorSP(RobotMap.DriveTrain.RIGHT_FRONT_MOTOR);
         rightRearMotor = new VictorSP(RobotMap.DriveTrain.RIGHT_REAR_MOTOR);
+        talon = new TalonSRX(0);
+        talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 0);
     }
 
     @Override
@@ -33,6 +36,10 @@ public class DriveTrain extends Subsystem {
         leftRearMotor.set(leftSpeed * (Constants.DriveTrain.LEFT_MOTORS_INVERTED ? -1 : 1));
         rightFrontMotor.set(rightSpeed * (Constants.DriveTrain.RIGHT_MOTORS_INVERTED ? -1 : 1));
         rightRearMotor.set(rightSpeed * (Constants.DriveTrain.RIGHT_MOTORS_INVERTED ? -1 : 1));
+    }
+
+    public void updateDashboard() {
+        SmartDashboard.putNumber("Arm/SRXMagEnc getSelectedSensorPosition", talon.getSelectedSensorPosition(0));
     }
 
 }
