d464c60e89043afe469325cdd8dd13b3bcfb2fc0
JCharante <git@jcharante.com>
2018-02-02 19:10:31 -0500

    add BufferBuffer

--- src/main/java/org/frc5687/powerup/prototyping/utils/JeVoisProxy.java
+++ src/main/java/org/frc5687/powerup/prototyping/utils/JeVoisProxy.java
@@ -3,7 +3,6 @@ package org.frc5687.powerup.prototyping.utils;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.frc5687.powerup.prototyping.Robot;
 import org.frc5687.powerup.prototyping.RobotMap;
 
 /**
@@ -37,27 +36,65 @@ public class JeVoisProxy {
         lastReadY = y;
     }
 
+    class BufferBuffer {
+        private String _buffer;
+        private String _delimeter;
+
+        public BufferBuffer(String delimeter) {
+            _buffer = "";
+            _delimeter = delimeter;
+        }
+
+        public void addToBuffer(String str) {
+            _buffer = _buffer.concat(str);
+        }
+
+        public boolean hasNext() {
+            return _buffer.contains(_delimeter);
+        }
+
+        public String next() {
+            if (hasNext()) {
+                int indexOf = _buffer.indexOf(_delimeter);
+                String packet = _buffer.substring(0, indexOf);
+                _buffer = _buffer.substring(indexOf + _delimeter.length(), _buffer.length());
+                return packet;
+            }
+            return "";
+        }
+    }
+
     protected class JeVoisListener implements Runnable {
         private SerialPort jevoisPort;
         private JeVoisProxy proxy;
 
+        private BufferBuffer buffer;
+
         protected JeVoisListener(JeVoisProxy proxy) {
             this.proxy = proxy;
             jevoisPort = new SerialPort(115200, RobotMap.JeVois.PORT);
+            char[] delim = new char[] {'\r', '\n'};
+            buffer = new BufferBuffer(new String(delim));
         }
 
         public void run() {
             while (true) {
                 try {
-                    String data = jevoisPort.readString();
-                    if (data.length() == 0) {
+                    String str = jevoisPort.readString();
+                    SmartDashboard.putString("JeVois/data_raw", str);
+                    buffer.addToBuffer(str);
+                    if (!buffer.hasNext()) {
+                        SmartDashboard.putBoolean("JeVois/buffer/hasNext", false);
                         continue;
+                    } else {
+                        SmartDashboard.putBoolean("JeVois/buffer/hasNext", true);
                     }
-                    SmartDashboard.putString("JeVois/data", data);
-                    String identifier_string = "JVTI:";
+                    String data = buffer.next();
+                    SmartDashboard.putString("JeVois/buffer/next", data);
+                    String identifier_string = "$:";
                     // Check to see if the identifier_string prepends our payload
                     if (data.length() <= identifier_string.length() || !data.substring(0, identifier_string.length()).equals(identifier_string)) {
-                        DriverStation.reportError(String.format("Rejected %s", data), false);
+                        //DriverStation.reportError(String.format("Rejected %s", data), false);
                         continue;
                     }
                     String payload = data.substring(identifier_string.length(), data.length());
@@ -69,9 +106,10 @@ public class JeVoisProxy {
                     int x = Integer.parseInt(a[0]);
                     int y = Integer.parseInt(a[1]);
                     proxy.Set(x, y);
-                    Thread.sleep(20);
+                    //Thread.sleep(1000 / 60);
+                    Thread.sleep(30);
                 } catch (Exception e) {
-                    continue;
+                    DriverStation.reportError(e.toString(), true);
                     // DriverStation.reportError(e.toString(), true);
                 }
             }
